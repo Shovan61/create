b8997a67e625d73fddcf2108e6f788394e7702d4
Shovan <shovonmazumder61@gmail.com>
2021-07-30 09:38:02 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Form from './Components/Form';
+import NewsList from './Components/NewsList';
+import Switch from '@material-ui/core/Switch';
+import { useGlobalContext } from './Context/context';
 import './App.css';
 
+const useStyles = makeStyles({
+    root: {},
+    footer: {
+        position: 'fixed',
+        bottom: '1rem',
+        right: '2.2rem',
+    },
+    switch: {
+        position: 'fixed',
+        top: '1rem',
+        right: '2.2rem',
+    },
+});
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const classes = useStyles();
+    const { isDarkMode, handletheme } = useGlobalContext();
+
+    useEffect(() => {
+        document.body.classList.toggle('dark', isDarkMode);
+    }, [isDarkMode]);
+
+    return (
+        <div className={`App ${isDarkMode && 'dark'}`}>
+            <div className={classes.switch}>
+                <Switch
+                    name='checkedA'
+                    inputProps={{ 'aria-label': 'secondary checkbox' }}
+                    onChange={handletheme}
+                />
+            </div>
+            <div className='header-container'>
+                <div
+                    className='emoji earth'
+                    role='img'
+                    aria-label='earth rotating'></div>
+                <h1
+                    className='header'
+                    style={{ color: `${isDarkMode ? 'red' : ''}` }}>
+                    Search News
+                </h1>
+            </div>
+            <Form />
+            <NewsList />
+            <div className={classes.footer}>
+                <span style={{ color: `${isDarkMode ? 'red' : ''}` }}>
+                    &copy;Powered by
+                </span>
+                :
+                <a href='https://hn.algolia.com/api' target='_blank'>
+                    <span style={{ color: `${isDarkMode ? 'green' : ''}` }}>
+                        HN Algolia
+                    </span>
+                </a>
+            </div>
+        </div>
+    );
 }
 
 export default App;
